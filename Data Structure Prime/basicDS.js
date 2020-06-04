2a596e05f96763a0d4ba56d00d76d0636f5992b8
aniket_bhavsar <aniket.bhavsar@azularc.com>
2020-06-04 07:33:20 +0530

    JS setup added

---
+++ Data Structure Prime/basicDS.js
@@ -0,0 +1,23 @@
+class Student {
+  constructor(fName, Lname) {
+    this.fName = fName;
+    this.Lname = Lname;
+    this.score = []
+  }
+
+  fullName(){
+    console.log("Hello Student Name is  " +this.fName,"",this.Lname);
+  }
+  calculateScore(){
+    let sumScore = this.score.reduce((a,b)=> a+b);
+    let  totalDigit = this.score.length;
+  console.log( "Average Score",Math.floor(sumScore/totalDigit));
+  }
+
+}
+
+let firstStudent = new Student ("Bunny","Steele");
+
+firstStudent.fullName();
+firstStudent.score.push(10,34,50);
+firstStudent.calculateScore();
